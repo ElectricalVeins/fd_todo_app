88a75d03f4190d5e7be4ee44539a8a4b48ec65c5
eveins <maxmost512@gmail.com>
2020-01-30 15:34:56 +0200

    add custom sequelize error handler

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,34 @@
 import express from 'express';
 import router from './routes';
-import errorHandler from './middlewares/errorHandler.js';
+import applicationErrorHandler
+  from './middlewares/error_handlers/applicationErrorHandler.js';
+import sequelizeErrorHandler
+  from './middlewares/error_handlers/sequelizeErrorHandler.js';
 
 const cors = require('cors');
 
+//Defining server port
 const PORT = process.env.PORT || 5000;
 
+//create server instance
 const app = express();
-app.use(cors());
-app.use(express.json());//Content-type: application/json
 
+//Set cors headers
+app.use(cors());
+
+// Allows Content-type: application/json
+app.use(express.json());
+
+//Routing
 app.use(router);
-app.use(errorHandler);
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+//Error Handling
+app.use(applicationErrorHandler);
+app.use(sequelizeErrorHandler);
+app.use((err, req, res) => {
+  res.status(500).send('Internal server error');
+});
+
+// Listening Start
+app.listen(PORT,
+           () => console.log(`Server app started. Listening on port ${PORT}!`));
