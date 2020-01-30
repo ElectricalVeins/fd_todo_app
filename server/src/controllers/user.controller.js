99dd6e1090ebf89f957bdc6f41ed0f21bfe9b5be
eveins <maxmost512@gmail.com>
2020-01-30 16:59:02 +0200

    Add MW for authorization

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,69 +1,71 @@
 import { User } from './../db/models';
 import AppErrors from '../utils/application_errors';
 
-export async function createUser( req, res, next ) {
-    try {
-        const createdUser = await User.create( req.userData );
-        if ( createdUser ) {
-            const data = createdUser.get();
-            delete data.password;
-            return res.status( 201 ).send( data );
-        }
-    } catch ( e ) {
-        next( new AppErrors.BadRequestError );
+export async function createUser (req, res, next) {
+  try {
+    const createdUser = await User.create(req.userData);
+    if (createdUser) {
+      const data = createdUser.get();
+      delete data.password;
+      return res.status(201).send(data);
     }
+    next(new AppErrors.BadRequestError());
+  } catch (e) {
+    next(e);
+  }
 }
 
-export async function getUserByPk( req, res, next ) {
-    try {
+export async function updateUser (req, res, next) {
+  try {
+    const [updatedRowsCount, updatedRows] = await User.update(req.userData, {
+      where: {
+        id: req.params.userId
+      },
+      returning: true,
+    });
 
-        const foundUser = await User.findByPk( req.params.userId, {
-            attributes: {
-                exclude: ['password']
-            }
-        } );
-
-        if ( foundUser ) {
-            return res.send( foundUser );
-        }
-        next( new Error() );
-    } catch ( e ) {
-        next( new AppErrors.ResourceNotFoundError );
+    if (updatedRowsCount) {
+      const data = updatedRows[0].get();
+      delete data.password;
+      return res.send(data);
     }
+    next(new AppErrors.ResourceNotFoundError('User'));
+  } catch (e) {
+    next(e);
+  }
 }
 
-export async function updateUser( req, res, next ) {
-    try {
-        const [updatedRowsCount, updatedRows] = await User.update( req.userData, {
-            where: {
-                id: req.params.userId
-            },
-            returning: true,
-        } );
+export async function getUserByPk (req, res, next) {
+  try {
 
-        if ( updatedRowsCount ) {
-            const data = updatedRows[ 0 ].get();
-            delete data.password;
-            return res.send( data );
-        }
-        next( new Error() );
-    } catch ( e ) {
-        next( new AppErrors.ResourceNotFoundError );
+    const foundUser = await User.findByPk(req.params.userId, {
+      attributes: {
+        exclude: ['password']
+      }
+    });
+
+    if (foundUser) {
+      return res.send(foundUser);
     }
+    next(new AppErrors.ResourceNotFoundError('User'));
+  } catch (e) {
+    next(e);
+  }
 }
 
-export async function deleteUserByPk( req, res, next ) {
-    try {
-        const deletedRowCount = await User.destroy( {
-                                                        where: {
-                                                            id: req.params.userId
-                                                        },
-                                                    } );
-        if ( deletedRowCount ) {
-            return res.send( `${ deletedRowCount }` );
-        }
-        next( '404 Resource not found' );
-    } catch ( e ) {
-        next( new AppErrors.ResourceNotFoundError );
+export async function deleteUserByPk (req, res, next) {
+  try {
+    const deletedRowCount = await User.destroy({
+
+                                                 where: {
+                                                   id: req.params.userId
+                                                 }
+                                               });
+    if (deletedRowCount) {
+      return res.send(`${deletedRowCount}`);
     }
+    next(new AppErrors.ResourceNotFoundError('User'));
+  } catch (e) {
+    next(e);
+  }
 }
