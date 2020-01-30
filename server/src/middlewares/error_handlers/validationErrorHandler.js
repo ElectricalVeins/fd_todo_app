9740d4391ac444d10dd0c7865e6bb2ac25ab0ef9
eveins <maxmost512@gmail.com>
2020-01-30 16:37:31 +0200

    add user validation error handler main function

--- server/src/middlewares/error_handlers/validationErrorHandler.js
+++ server/src/middlewares/error_handlers/validationErrorHandler.js
@@ -1,8 +1,26 @@
-import {ValidationError} from '@hapi/joi';
+import { ValidationError } from '@hapi/joi';
 
-export default (err, req,res,next)=>{
+export default (err, req, res, next) => {
   if (err instanceof ValidationError) {
-  return res.status(400).send(`${err.details[0].path[0]} value is not valid`);
+    const { details: [errorDetails] } = err;
+
+    const label = errorDetails.context.label;
+    let errorMessage = '';
+
+    switch (errorDetails.type) {
+      case 'any.required':
+        errorMessage = `${label} is required`;
+        break;
+      case 'string.pattern.base':
+        errorMessage = `${label} with value \"${errorDetails.context.value}\" fails to match the required pattern`;
+        break;
+      default:
+        errorMessage = 'Validation failed';
+        break;
+
+    }
+
+    return res.status(400).send(errorMessage);
   }
   next(err);
 }
