03a6fd7d69de7b6907dc6af1ea60767512ca03d2
eveins <maxmost512@gmail.com>
2020-01-31 14:27:25 +0200

    add task router initial setup

---
+++ server/src/controllers/task.controller.js
@@ -0,0 +1,7 @@
+import { User,Task } from './../db/models';
+import AppErrors from '../utils/application_errors';
+
+
+export async function createUser (req, res, next) {
+
+}
---
+++ server/src/routes/task.router.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const taskRouter = express.Router();
+
+
+taskRouter.post('/',);
+
+taskRouter.get('/:taskId',);
+
+taskRouter.patch('/:taskId',);
+
+taskRouter.delete('/:taskId',);
+
+
+export default taskRouter;
