b7fdb62250b80607a8c06db849b9c053f56365fc
eveins <maxmost512@gmail.com>
2020-01-31 14:06:03 +0200

    refactor validation error handler

--- server/src/middlewares/error_handlers/validationErrorHandler.js
+++ server/src/middlewares/error_handlers/validationErrorHandler.js
@@ -3,24 +3,10 @@ import { ValidationError } from '@hapi/joi';
 export default (err, req, res, next) => {
   if (err instanceof ValidationError) {
     const { details: [errorDetails] } = err;
-
     const label = errorDetails.context.label;
-    let errorMessage = '';
-
-    switch (errorDetails.type) {
-      case 'any.required':
-        errorMessage = `${label} is required`;
-        break;
-      case 'string.pattern.base':
-        errorMessage = `${label} with value \"${errorDetails.context.value}\" fails to match the required pattern`;
-        break;
-      default:
-        errorMessage = 'Validation failed';
-        break;
-
-    }
-
-    return res.status(400).send(errorMessage);
+    return res.status(400).send(errorDetails.type === 'string.pattern.base'
+                                ? `${label} with value \"${errorDetails.context.value}\" fails to match the required pattern`
+                                : errorDetails.message);
   }
   next(err);
 }
