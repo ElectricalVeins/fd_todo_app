7934474b4a8c94c40960695d9496d28a5cf3e89f
eveins <maxmost512@gmail.com>
2020-02-03 14:55:45 +0200

    Refactor role model
    Change role type to enum

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes';
 import errorHandlers from './middlewares/error_handlers'
 
-const cors = require('cors');
 
 //Defining server port
 const PORT = process.env.PORT || 5000;
