eec4b0ebe09fb154a014041acfd478a8b9d18852
eveins <maxmost512@gmail.com>
2020-02-03 14:51:38 +0200

    create: enums actions, roles, entities

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,17 +1,68 @@
-export const USER_NAME_PATTERN =/^[A-Z][a-z]{0,63}$/;
+export const USER_NAME_PATTERN = /^[A-Z][a-z]{0,63}$/;
 export const LOGIN_PATTERN = /(?!^\d)^\w{6,16}$/;
 export const PASSWORD_PATTERN = /(?=.*\d)(?=.*?[a-z])(?=.*?[A-Z])^[a-zA-Z0-9_$@!#%?]{8,60}$/;
 
-export const ROLES={
-  OWNER:'OWNER',
-  USER:'USER',
-  MODERATOR:'MODERATOR',
-  ADMIN:'ADMIN',
-};
+/**
+ *
+ * @typedef {Symbol} Action
+ */
 
-export const ACTIONS={
-  CREATE:'CREATE',
-  READ:'READ',
-  UPDATE:'UPDATE',
-  DELETE:'DELETE',
-};
+/**
+ *
+ * @enum {Action} Actions
+ */
+export const ACTIONS = Object.freeze({
+                                       /**
+                                        * @readonly
+                                        */
+                                       CREATE: Symbol('CREATE'),
+                                       /**
+                                        * @readonly
+                                        */
+                                       READ: Symbol('READ'),
+                                       /**
+                                        * @readonly
+                                        */
+                                       UPDATE: Symbol('UPDATE'),
+                                       /**
+                                        * @readonly
+                                        */
+                                       DELETE: Symbol('DELETE'),
+                                     });
+/**
+ *
+ * @typedef {string} Role
+ */
+
+/**
+ *
+ * @enum {Role} Roles
+ */
+export const ROLES = Object.freeze({
+                                     /**
+                                      * @readonly
+                                      */
+                                     ADMIN: 'ADMIN',
+                                     /**
+                                      * @readonly
+                                      */
+                                     MODERATOR: 'MODERATOR',
+                                     /**
+                                      * @readonly
+                                      */
+                                     USER: 'USER',
+                                   });
+
+/**
+ *@typedef {Symbol} Entity
+ */
+
+/**
+ * @enum {Entity} Entities
+ */
+export const ENTITIES = Object.freeze({
+                                        TASK: 'TASK',
+                                        USER: 'USER',
+                                      });
+
+
