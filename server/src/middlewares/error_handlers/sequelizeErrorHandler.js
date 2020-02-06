2a6c9e8bc0069d6666f73671179c89c8e055bd07
eveins <maxmost512@gmail.com>
2020-02-06 16:59:40 +0200

    bugfix Sequelize Unique Constraint Error Handler

--- server/src/middlewares/error_handlers/sequelizeErrorHandler.js
+++ server/src/middlewares/error_handlers/sequelizeErrorHandler.js
@@ -1,23 +1,25 @@
 import { BaseError, ValidationError, UniqueConstraintError } from 'sequelize';
 
 export default (err, req, res, next) => {
-  /*
+
     switch (err) {
       case err instanceof UniqueConstraintError :
-        res.status(400).send(err.errors[0]);
+    res.status(400).send(
+      `Error: ${err.errors[0].message}. This ${err.errors[0].path} was already registered.`);
         break;
       case err instanceof BaseError:
         res.status(400).send(err);
         break;
-    }*/
+    }
 
-  if (err instanceof UniqueConstraintError) {
+
+/*  if (err instanceof UniqueConstraintError) {
     res.status(400).send(
       `Error: ${err.errors[0].message}. This ${err.errors[0].path} was already registered.`);
     return;
   }
   if (err instanceof BaseError) {
     res.status(400).send(err);
-  }
+  }*/
   next(err);
 }
